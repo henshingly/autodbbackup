4a7dfe4b3fb084f7c0545cc2c3705c89b0dc9c1d
Henshingly <dietmar_kersting@web.de>
2024-09-22 13:18:54 +0200

    Update german
    Update german translation for Casual Honorifics

--- language/de/info_acp_auto_db_backup.php
+++ language/de/info_acp_auto_db_backup.php
@@ -27,10 +27,10 @@ $lang = array_merge($lang, array(
 	'LOG_AUTO_DB_BACKUP'					=> '<strong>Automatische Datenbanksicherung abgeschlossen</strong>',
 	'LOG_AUTO_DB_BACKUP_SETTINGS_CHANGED'	=> '<strong>Geänderte Einstellungen für die automatische Datenbanksicherung</strong>',
 	//Donation
-	'PAYPAL_IMAGE_URL'          => 'https://www.paypalobjects.com/webstatic/en_US/i/btn/png/silver-pill-paypal-26px.png',
-	'PAYPAL_ALT'                => 'Spende mit PayPal',
-	'BUY_ME_A_BEER_URL'         => 'https://paypal.me/RMcGirr83',
+	'PAYPAL_IMAGE_URL'			=> 'https://www.paypalobjects.com/webstatic/en_US/i/btn/png/silver-pill-paypal-26px.png',
+	'PAYPAL_ALT'				=> 'Spende über PayPal',
+	'BUY_ME_A_BEER_URL'			=> 'https://paypal.me/RMcGirr83',
 	'BUY_ME_A_BEER'				=> 'Für die Erstellung dieser Erweiterung könntest Du mir ja ein Bier ausgeben',
 	'BUY_ME_A_BEER_SHORT'		=> 'Spende doch für diese Erweiterung',
-	'BUY_ME_A_BEER_EXPLAIN'		=> 'Diese Erweiterung ist völlig kostenlos. Es ist ein Projekt, mit dem ich meine Zeit zum Vergnügen und zur Nutzung der phpBB-Community verbringe. Wenn Du diese Erweiterung gerne verwendest oder Dein Forum davon profitiert hat, denke doch bitte daran <a href="https://paypal.me/RMcGirr83" target="_blank" rel="noreferrer noopener">mir ein 	 auszugeben</a>. Ich würde es sehr zu schätzen wissen: <i class="fa fa-smile-o" style="color:green;font-size:1.5em;" aria-hidden="true"></i>',
+	'BUY_ME_A_BEER_EXPLAIN'		=> 'Diese Erweiterung ist vollkommen kostenlos. Es ist ein Projekt, mit dem ich meine Zeit zum Vergnügen und zur Nutzung der phpBB-Community verbringe. Wenn Du diese Erweiterung gerne verwendest oder Dein Forum davon profitiert hat, denke doch bitte daran <a href="https://paypal.me/RMcGirr83" target="_blank" rel="noreferrer noopener">mir einBier auszugeben</a>. Ich wäre sehr dankbar. <i class="fa fa-smile-o" style="color:green;font-size:1.5em;" aria-hidden="true"></i>',
 ));
